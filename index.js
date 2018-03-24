15a0952211701d0e0b05f043f7da7e73e42c1554
Luis Gelvis <luisgelvis123@gmail.com>
2018-03-24 16:55:28 -0500

    primer commit

---
+++ index.js
@@ -0,0 +1,18 @@
+const electron = require('electron');
+const {app,BrowserWindow} = electron;
+
+const path = require('path');
+const url = require('url');
+
+let win = null
+
+app.on('ready', function(){
+    win = new BrowserWindow({width:500, height:400})
+    win.loadURL(
+        url.format({
+            pathname: path.join(__dirname, 'index.html'),
+            protocol: 'file',
+            slashes: true
+        })
+    )
+})
