1f43dadb54dd72581b1436d5ae69dded9fe2dffc
Luis Gelvis <luisgelvis123@gmail.com>
2018-03-24 17:43:53 -0500

    login frontend

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const url = require('url');
 let win = null
 
 app.on('ready', function(){
-    win = new BrowserWindow({width:500, height:400})
+    win = new BrowserWindow({width:500, height:800})
     win.loadURL(
         url.format({
             pathname: path.join(__dirname, 'index.html'),
