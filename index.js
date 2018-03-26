aff772c304909046ff987b9e24b6060db5d6cf50
Luis Gelvis <luisgelvis123@gmail.com>
2018-03-26 11:40:24 -0500

    Frontend Usuario Tabla Datos 80%

--- index.js
+++ index.js
@@ -2,18 +2,16 @@ const electron = require('electron');
 const {app,BrowserWindow} = electron;
 
 const path = require('path');
-
 const url = require('url');
-var main = {a: 1,b: 2}; module.exports = main; 
 let win = null
 
 app.on('ready', function(){
     
-    win = new BrowserWindow({width:600, height:800, icon: __dirname + '/images/icons/favicon.icoo'})
+    win = new BrowserWindow({width:1200, height:800, icon: __dirname + '/images/icons/favicon.icoo'})
     win.setMenu(null);
     win.loadURL(
         url.format({
-            pathname: path.join(__dirname, 'index.html'),
+            pathname: path.join(__dirname, 'View/html/index.html'),
             protocol: 'file',
             slashes: true
         })
