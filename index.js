607afebfb19fbb38de3de8e96fc108cc9e943da8
Luis Gelvis <luisgelvis123@gmail.com>
2018-03-26 08:49:33 -0500

    login frontend 100%

--- index.js
+++ index.js
@@ -2,13 +2,14 @@ const electron = require('electron');
 const {app,BrowserWindow} = electron;
 
 const path = require('path');
-const url = require('url');
 
+const url = require('url');
+var main = {a: 1,b: 2}; module.exports = main; 
 let win = null
 
 app.on('ready', function(){
     
-    win = new BrowserWindow({width:500, height:800, icon: __dirname + '/images/icons/favicon.icoo'})
+    win = new BrowserWindow({width:600, height:800, icon: __dirname + '/images/icons/favicon.icoo'})
     win.setMenu(null);
     win.loadURL(
         url.format({
