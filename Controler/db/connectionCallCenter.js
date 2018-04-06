0df74d4b1814aedbe527d1ceb99b89a9a5a42410
Luis Gelvis <luisgelvis123@gmail.com>
2018-04-06 17:28:12 -0500

    Login Modify

--- Controler/db/connectionCallCenter.js
+++ Controler/db/connectionCallCenter.js
@@ -3,16 +3,17 @@ var mysql = require('mysql');
 
 //Configuración necesaria para conectar a la base de datos
 var connection = mysql.createConnection({
-    /*
+   
     host: '192.168.1.23',
     user: 'usuario1',
     password: '1qaz18++',
     database : 'callcenter',
     port: 3306
-    */
-    host: 'localhost',
+    
+    /* host: 'localhost',
     user: 'root',
     password: '',
     database : 'callcenter',
     port: 3306
+    */
 });
